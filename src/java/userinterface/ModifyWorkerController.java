ac5d66698e4e88faa23bdc137fc0bec8a1c6c99f
Kevin Hayden <kevphayden@gmail.com>
2017-03-08 02:22:14 -0500

    Kevin Fifth Commit

--- src/java/userinterface/ModifyWorkerController.java
+++ src/java/userinterface/ModifyWorkerController.java
@@ -1,7 +1,20 @@
 package userinterface;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by kevph on 3/7/2017.
  */
-public class ModifyWorkerController {
+public class ModifyWorkerController implements Initializable {
+
+    public ModifyWorkerController() {
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
