8e05b1a50b9b0ddf4ad679f9b95da8bc6cffcaa4
Ylies Chahi <ylieschahi@gmail.com>
2017-03-09 18:18:21 -0500

    creation of models

---
+++ src/java/models/MaxDueDate.java
@@ -0,0 +1,17 @@
+package models;
+
+public class MaxDueDate {
+
+    String currentMaxDueDate;
+
+    public void MaxDueDate() {
+        this.currentMaxDueDate = "";
+    }
+
+    public String getCurrentMaxDueDate() {return currentMaxDueDate;}
+
+    public void setCurrentMaxDueDate(String currentMaxDueDate) {
+        this.currentMaxDueDate = currentMaxDueDate;
+    }
+}
+
