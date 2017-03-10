1cda93e596f3a231a4d60e8d706c6ca2bfe44575
Oleg Gorbatchev <oleg.gorbatchev@gmail.com>
2017-03-09 22:47:46 -0500

    [SAVE] merging all group member tasks

--- src/java/userinterface/ModifyBookController.java
+++ src/java/userinterface/ModifyBookController.java
@@ -1,7 +1,21 @@
 package userinterface;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by kevph on 3/7/2017.
  */
-public class ModifyBookController {
+public class ModifyBookController implements Initializable {
+
+
+    public ModifyBookController() {
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- src/java/userinterface/ModifyStudentController.java
+++ src/java/userinterface/ModifyStudentController.java
@@ -1,7 +1,21 @@
 package userinterface;
 
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by kevph on 3/7/2017.
  */
-public class ModifyStudentController {
+public class ModifyStudentController implements Initializable {
+
+
+    public ModifyStudentController() {
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
