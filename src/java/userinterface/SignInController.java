63d56656c1802a472b40c6886172225f8213ffb3
alindquist <awlindquist@gmail.com>
2017-03-25 01:36:08 -0400

    Fixed skip method w/ dope new features

--- src/java/userinterface/SignInController.java
+++ src/java/userinterface/SignInController.java
@@ -18,10 +18,11 @@ import utilities.Core;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 /**
- * Created by Kevin on 3/8/2017.
+ * Created by Kevin, modified by Andeezy on 3/8/2017.
  */
 public class SignInController implements Initializable {
 
@@ -75,17 +76,82 @@ public class SignInController implements Initializable {
         }
     }
 
-    /**
-     * @param actionEvent
-     */
-    public void skip(ActionEvent actionEvent) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainview.fxml"));
-        Stage primaryStage = new Stage();
-        Scene scene = new Scene(root);
-        primaryStage.getIcons().add(new Image("https://upload.wikimedia.org/wikipedia/en/e/ef/Brockp_Gold_Eagles_logo.png"));
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Brockport Library System!");
-        primaryStage.setResizable(false);
-        primaryStage.show();
+
+    public void skipAsAdmin(ActionEvent actionEvent) throws IOException {
+
+        dummyWorker(1);
+
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainview.fxml"));
+            Stage primaryStage = new Stage();
+            Scene scene = new Scene(root);
+            primaryStage.getIcons().add(new Image("https://upload.wikimedia.org/wikipedia/en/e/ef/Brockp_Gold_Eagles_logo.png"));
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Brockport Library System!");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            System.out.println("Can't open new window.");
+        }
+    }
+
+
+    public void skipAsOrdinary(ActionEvent actionEvent) {
+
+        dummyWorker(2);
+
+        try {
+
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainview.fxml"));
+            Stage primaryStage = new Stage();
+            Scene scene = new Scene(root);
+            primaryStage.getIcons().add(new Image("https://upload.wikimedia.org/wikipedia/en/e/ef/Brockp_Gold_Eagles_logo.png"));
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Brockport Library System!");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            System.out.println("Can't open new window.");
+        }
+    }
+
+
+    private void dummyWorker(int type) {
+
+        String[] workerArray = {"BannerId", "Password", "FirstName", "LastName", "ContactPhone",
+                "Email", "Credentials", "DateOfLatestCredentialsStatus", "DateOfHire", "Status"};
+
+        String[] workerData;
+
+        if (type == 1) {
+
+            workerData = new String[]{"800123456", "123456", "John", "Doe", "555-666-1234", "DaMan@gmail.com",
+                    "Administrator", "3-21-2017", "4-17-2007", "Active"};
+
+            Properties props = new Properties();
+            for (int i = 0; i < workerArray.length; i++) {
+                props.put(workerArray[i], workerData[i]);
+            }
+
+            Worker worker = new Worker(props);
+            core.setUser(worker);
+
+        } else if (type == 2) {
+
+            workerData = new String[]{"800123456", "123456", "John", "Doe", "555-666-1234", "DaMan@gmail.com",
+                    "Ordinary", "3-21-2017", "4-17-2007", "Active"};
+
+            Properties props = new Properties();
+            for (int i = 0; i < workerArray.length; i++) {
+                props.put(workerArray[i], workerData[i]);
+            }
+
+            Worker worker = new Worker(props);
+            core.setUser(worker);
+
+        }
     }
 }
