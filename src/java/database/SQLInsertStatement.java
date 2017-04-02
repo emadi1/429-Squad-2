743f28497b6cd0a0550709019b15b1eaed7fd973
kevtr0n <kevphayden@gmail.com>
2017-04-02 03:23:25 -0400

    "Minor tweaks"

--- src/java/database/SQLInsertStatement.java
+++ src/java/database/SQLInsertStatement.java
@@ -85,12 +85,12 @@ public class SQLInsertStatement extends SQLStatement
 			//System.out.println("Checking insertType");
 			String insertType = schema.getProperty(theColumnName);
 			//		System.out.println("InsertType = " + insertType);
-			System.out.println("Schema is : " + schema);
+			//System.out.println("Schema is : " + schema);
 
 			if (insertType.equals("numeric") == true)
 			{
 				theValuesString += theColumnValue;
-					System.out.println("Value string updated: " + theValuesString);
+			//		System.out.println("Value string updated: " + theValuesString);
 			}
 			else
 			{
