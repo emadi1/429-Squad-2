da2feaf98c2ffa7b74193fba639ece62f3f5d8e4
Spencer Linsner <sjlinsner@gmail.com>
2017-04-25 17:26:03 -0400

    Initial playing-around with JUnit

---
+++ src/test/models/BookTest.java
@@ -0,0 +1,49 @@
+package models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Spencer on 4/18/2017.
+ */
+public class BookTest {
+    @Before
+    public void setUp() throws Exception {
+        Properties p1 = new Properties();
+        Book b1 = new Book(p1);
+    }
+
+    @Test
+    public void testCompare() throws Exception {
+
+    }
+
+    @Test
+    public void testFormatPrice() throws Exception {
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+    }
+
+    @Test
+    public void testGenerateDiscipline() throws Exception {
+    }
+
+    @Test
+    public void testToString() throws Exception {
+    }
+
+    @Test
+    public void testToolTipToString() throws Exception {
+    }
+
+}
