55d8f73516d0ffe8bdf333a8131720c3ff757cf1
Anders Lindquist <awlindquist@gmail.com>
2017-04-25 18:16:49 -0400

    PW Encryption 1/2 complete

--- src/java/userinterface/AddWorkerViewController.java
+++ src/java/userinterface/AddWorkerViewController.java
@@ -9,6 +9,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
+import javax.crypto.*;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.InvalidParameterSpecException;
+import java.util.Base64;
+
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 import models.Worker;
@@ -48,6 +62,17 @@ public class AddWorkerViewController implements Initializable {
     @FXML private ComboBox<String> Status;
     @FXML private Button submit;
     @FXML private Text alertMessage;
+
+    Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    byte[] salt = new String("12345678").getBytes();
+    int keyLength = 128;
+    int iterations = 40000;
+    String masterPassword = "thisisnotsafe";
+    SecretKeySpec key = createKey(masterPassword.toCharArray(), salt, iterations, keyLength);
+
+
+    public AddWorkerViewController() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException {
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -96,7 +121,7 @@ public class AddWorkerViewController implements Initializable {
         Status.setValue(lang.getProperty("Active"));
         Status.setItems(statusList);
     }
-    public void submit(ActionEvent event) {
+    public void submit(ActionEvent event) throws IllegalBlockSizeException, BadPaddingException, InvalidParameterSpecException, InvalidKeyException, UnsupportedEncodingException {
 
         Properties prop = new Properties();
         WorkerCollection workerCollection = new WorkerCollection();
@@ -143,6 +168,7 @@ public class AddWorkerViewController implements Initializable {
         if (count == 0) {
             prop.put(Status.getId(), Status.getSelectionModel().getSelectedItem());
             prop.put(Credentials.getId(), Credentials.getSelectionModel().getSelectedItem());
+            prop.put("Password", encrypt(Password.getText(), key, cipher));
             Worker newWorker = new Worker(prop);
             newWorker.insert();
             alertMessage.setText(lang.getProperty("addWorkerSuccess"));
@@ -151,5 +177,28 @@ public class AddWorkerViewController implements Initializable {
         for (TextField t : textFieldList) t.clear();
         Credentials.setValue(lang.getProperty("Ordinary"));
         Status.setValue(lang.getProperty("Active"));
+    }
+
+
+    private static SecretKeySpec createKey (char[] password, byte[] salt, int iteration, int keyLength) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iteration, keyLength);
+        SecretKey tempKey = keyFactory.generateSecret(keySpec);
+        return new SecretKeySpec(tempKey.getEncoded(), "AES");
+    }
+
+
+    private static String encrypt(String unencryptedPass, SecretKeySpec key, Cipher cipher) throws InvalidKeyException, InvalidParameterSpecException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        AlgorithmParameters parameters = cipher.getParameters();
+        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+        byte[] cryptoText = cipher.doFinal(unencryptedPass.getBytes("UTF-8"));
+        byte[] iv = ivParameterSpec.getIV();
+        return base64Encode(iv) + ":" + base64Encode(cryptoText);
+    }
+
+
+    private static String base64Encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
     }
 }
